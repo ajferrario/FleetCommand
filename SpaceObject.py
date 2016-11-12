1d4bee4c362cfdb10e3b907d2993d7e046613286
ajferrario <anthony.ferrario@gmail.com>
2016-11-11 23:16:50 -0800

    Adding in the first 2 base classes

---
+++ SpaceObject.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""Define the SpaceObject class"""
+class SpaceObject:
+
+    """Represent an object in space.
+
+    Provide a base class which has the primary attributes and functions of any object that could be present in a space
+    environment. The 
+
+
+
+    """
+
+    __counter = 0
+
+    def __init__(self):
